8233b146d874d5a3517e7e9862d2cb2b1b05f75b
J. v. Roos <info@jvr.de>
2021-07-14 12:47:03 +0200

    0.003

--- create_startdelayed_link_in_autostartfolder.bat
+++ create_startdelayed_link_in_autostartfolder.bat
@@ -7,4 +7,3 @@
 @powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\%app%.lnk');$s.TargetPath='%~dp0\%app%.bat';$s.Save()"
 
 
-
